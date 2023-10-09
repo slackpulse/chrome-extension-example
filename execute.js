370ff8aff72239c4b5761d8698ce7ab437988912
slackpulse <slackpulse@gmail.com>
2023-10-09 22:48:05 +0900

    Dアニメストアのお知らせを取得

--- execute.js
+++ execute.js
@@ -1,4 +1,15 @@
-// this code will be executed when the extension's button is clicked
-(function() {
-  console.log('execute.js executed');
-})();
+// https://animestore.docomo.ne.jp/animestore/tp_pc
+const targets = document.querySelectorAll('ul.newsBlock.news')
+for (const target of targets) {
+  const observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      mutation.addedNodes.forEach(function(node) {
+        console.log(node.textContent)
+      })
+    });
+  })
+  const options = {
+    childList: true,
+  }
+  observer.observe(target, options);
+}
