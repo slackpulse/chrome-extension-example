fba55e060162346e99f2117e5f2da0cbb96cf994
slackpulse <slackpulse@gmail.com>
2023-10-11 02:34:10 +0900

    content_scriptsから読み込むようにした

--- execute.js
+++ execute.js
@@ -1,14 +1,7 @@
-const video = document.querySelector("#video")
-const showInfo = function(event) {
-  const backInfoTxt1 = document.querySelector(".backInfoTxt1")
-  const backInfoTxt2 = document.querySelector(".backInfoTxt2")
-  const backInfoTxt3 = document.querySelector(".backInfoTxt3")
+const backInfoTxt1 = document.querySelector(".backInfoTxt1")
+const backInfoTxt2 = document.querySelector(".backInfoTxt2")
+const backInfoTxt3 = document.querySelector(".backInfoTxt3")
 
-  console.log(`backInfoTxt1: ${backInfoTxt1.textContent}`)
-  console.log(`backInfoTxt2: ${backInfoTxt2.textContent}`)
-  console.log(`backInfoTxt3: ${backInfoTxt3.textContent}`)
-}
-
-video.addEventListener('ended', showInfo)
-
-showInfo()
+console.log(`backInfoTxt1: ${backInfoTxt1.textContent}`)
+console.log(`backInfoTxt2: ${backInfoTxt2.textContent}`)
+console.log(`backInfoTxt3: ${backInfoTxt3.textContent}`)
